4ad1e660dcebc5956bc32b474d4e95651a8c3b62
Alin-Daniel Alin-Daniel <daniel.alin100@gmail.com>
2020-02-22 12:05:27 +0200

    Updates

---
+++ precache-manifest.989499d574436ba226ddff78ec31d1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c6cacd28ba0bd3e6d4382064afee3e",
+    "url": "/simple-blog/index.html"
+  },
+  {
+    "revision": "30deadfcd26f6afa68d8",
+    "url": "/simple-blog/static/css/main.575a9d92.chunk.css"
+  },
+  {
+    "revision": "34a244276eae00c5a5fa",
+    "url": "/simple-blog/static/js/2.35fa91c4.chunk.js"
+  },
+  {
+    "revision": "30deadfcd26f6afa68d8",
+    "url": "/simple-blog/static/js/main.1f577f02.chunk.js"
+  },
+  {
+    "revision": "d1f0958660b969eb0fd4",
+    "url": "/simple-blog/static/js/runtime~main.5e2413b1.js"
+  },
+  {
+    "revision": "80687d23d8eef70e255e1996a06d9f5c",
+    "url": "/simple-blog/static/media/react-logo.80687d23.png"
+  }
+]);
